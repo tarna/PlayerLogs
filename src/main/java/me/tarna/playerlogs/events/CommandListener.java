65254be6a07a503d31e74cfe33c53d57946ade00
tarna <46247728+tarna@users.noreply.github.com>
2021-03-29 04:14:28 -0400

    Update CommandListener.java

--- src/main/java/me/tarna/playerlogs/events/CommandListener.java
+++ src/main/java/me/tarna/playerlogs/events/CommandListener.java
@@ -1,5 +1,6 @@
 package me.tarna.playerlogs.events;
 
+import me.tarna.playerlogs.PlayerLogs;
 import me.tarna.playerlogs.lib.Log;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,10 +9,11 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 public class CommandListener implements Listener {
 
     Log log;
+    PlayerLogs main = PlayerLogs.instance;
 
     @EventHandler
     public void OnCommand(PlayerCommandPreprocessEvent e) {
-
+        if(!main.getConfig().getBoolean("commands")) return;
         String msg = e.getPlayer().getName() + " executed command " + e.getMessage();
         log = new Log("command", msg);
 
