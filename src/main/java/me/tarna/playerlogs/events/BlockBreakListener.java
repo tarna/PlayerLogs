c47347e1f0c0e570f3afc448bf72729b1e8b36de
tarna <46247728+tarna@users.noreply.github.com>
2021-03-29 04:13:08 -0400

    Update BlockBreakListener.java

--- src/main/java/me/tarna/playerlogs/events/BlockBreakListener.java
+++ src/main/java/me/tarna/playerlogs/events/BlockBreakListener.java
@@ -1,5 +1,6 @@
 package me.tarna.playerlogs.events;
 
+import me.tarna.playerlogs.PlayerLogs;
 import me.tarna.playerlogs.lib.Log;
 import me.tarna.playerlogs.lib.Util;
 import org.bukkit.block.Block;
@@ -11,9 +12,11 @@ public class BlockBreakListener implements Listener {
 
     Util util = new Util();
     Log log;
+    PlayerLogs main = PlayerLogs.instance;
 
     @EventHandler
     public void OnBreak(BlockBreakEvent e) {
+        if(!main.getConfig().getBoolean("block-break")) return;
         Block b = e.getBlock();
         String msg = e.getPlayer().getName() + " broke " + b.getType().toString() + " " + util.LocationToText(b.getLocation());
         log = new Log("block-break", msg);
