31bd38f91e449ccd084f24745ad9268760d0879b
tarna <46247728+tarna@users.noreply.github.com>
2021-03-29 04:14:52 -0400

    Update JoinListener.java

--- src/main/java/me/tarna/playerlogs/events/JoinListener.java
+++ src/main/java/me/tarna/playerlogs/events/JoinListener.java
@@ -1,5 +1,6 @@
 package me.tarna.playerlogs.events;
 
+import me.tarna.playerlogs.PlayerLogs;
 import me.tarna.playerlogs.lib.Log;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,10 +9,11 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinListener implements Listener {
 
     Log log;
+    PlayerLogs main = PlayerLogs.instance;
 
     @EventHandler
     public void OnJoin(PlayerJoinEvent e) {
-
+        if(!main.getConfig().getBoolean("join")) return;
         String msg = e.getPlayer().getName() + " joined the server";
         log = new Log("join", msg);
 
